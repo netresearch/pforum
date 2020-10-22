bcca112a9906670a0b7bb3340e0cdc815ebeb888
Stefan Froemken <froemken@gmail.com>
2020-10-22 18:43:47 +0200

    Move TCA related modification into TCA/Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,16 @@
 <?php
-// load tt_content to $TCA array and add flexform
-$pluginSignature = 'pforum_forum';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:pforum/Configuration/FlexForms/Forum.xml');
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.pforum',
+    'Forum',
+    'LLL:EXT:pforum/Resources/Private/Language/locallang_db.xlf:plugin.title'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['pforum_forum'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'pforum_forum',
+    'FILE:EXT:pforum/Configuration/FlexForms/Forum.xml'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -4,40 +4,17 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'JWeiland.'.$_EXTKEY,
-    'Forum',
-    'LLL:EXT:pforum/Resources/Private/Language/locallang_db.xlf:plugin.title'
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+    'JWeiland.pforum',
+    'web',
+    'forum',
+    '',
+    [
+        'Forum' => 'listHidden, activate',
+    ],
+    [
+        'access' => 'user,group',
+        'icon' => 'EXT:pforum/Resources/Public/Icons/module.svg',
+        'labels' => 'LLL:EXT:pforum/Resources/Private/Language/locallang_forum.xlf',
+    ]
 );
-
-if (TYPO3_MODE === 'BE') {
-    /*
-     * Registers a Backend Module
-     */
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'JWeiland.'.$_EXTKEY,
-        'web',     // Make module a submodule of 'web'
-        'forum',    // Submodule key
-        '',                        // Position
-        [
-            'Forum' => 'listHidden, activate',
-        ],
-        [
-            'access' => 'user,group',
-            'icon' => 'EXT:'.$_EXTKEY.'/Resources/Public/Icons/module.svg',
-            'labels' => 'LLL:EXT:'.$_EXTKEY.'/Resources/Private/Language/locallang_forum.xlf',
-        ]
-    );
-}
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pforum_domain_model_forum', 'EXT:pforum/Resources/Private/Language/locallang_csh_tx_pforum_domain_model_forum.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pforum_domain_model_forum');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pforum_domain_model_topic', 'EXT:pforum/Resources/Private/Language/locallang_csh_tx_pforum_domain_model_topic.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pforum_domain_model_topic');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pforum_domain_model_post', 'EXT:pforum/Resources/Private/Language/locallang_csh_tx_pforum_domain_model_post.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pforum_domain_model_post');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pforum_domain_model_anonymoususer', 'EXT:pforum/Resources/Private/Language/locallang_csh_tx_pforum_domain_model_anonymoususer.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pforum_domain_model_anonymoususer');
