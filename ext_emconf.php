bba272c329820ac2f6d2b68fc4db349264c38b12
Stefan Froemken <froemken@gmail.com>
2022-10-19 11:53:17 +0200

    Remove TYPO3 9 compatibility. Add TYPO3 11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.20-10.4.99',
+            'typo3' => '10.4.32-11.5.16',
         ],
         'conflicts' => [
         ],
