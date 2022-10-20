338d9c12a8766dd9c9cf8d9fdb0e2832dfce397e
Stefan Froemken <froemken@gmail.com>
2022-10-20 13:08:20 +0200

    Move methods of abstract classes into topic/post controller

--- Classes/Controller/PostController.php
+++ Classes/Controller/PostController.php
@@ -15,13 +15,15 @@ use JWeiland\Pforum\Domain\Model\Post;
 use JWeiland\Pforum\Domain\Model\Topic;
 use JWeiland\Pforum\Domain\Model\User;
 use TYPO3\CMS\Core\Mail\MailMessage;
+use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Extbase\Annotation as Extbase;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
  * Controller to manage (list and show) postings
  */
-class PostController extends AbstractPostController
+class PostController extends AbstractController
 {
     /**
      * @param Topic $topic
@@ -251,5 +253,89 @@ class PostController extends AbstractPostController
 
         $this->addFlashMessage(LocalizationUtility::translate('postActivated', 'pforum'));
         $this->redirect('list', 'Forum');
+    }
+
+    /**
+     * @var PageRenderer
+     */
+    protected $pageRenderer;
+
+    public function injectPageRenderer(PageRenderer $pageRenderer): void
+    {
+        $this->pageRenderer = $pageRenderer;
+    }
+
+    /**
+     * This is a workaround to help controller actions to find (hidden) posts.
+     */
+    protected function registerPostFromRequest(string $argumentName): void
+    {
+        $argument = $this->request->getArgument($argumentName);
+        if (is_array($argument)) {
+            // get post from form ($_POST)
+            $post = $this->postRepository->findHiddenEntryByUid((int)$argument['__identity']);
+        } else {
+            // get post from UID
+            $post = $this->postRepository->findHiddenEntryByUid((int)$argument);
+        }
+        $this->session->registerObject($post, $post->getUid());
+    }
+
+    protected function addFeUserToPost(Topic $topic, Post $newPost): void
+    {
+        if (is_array($GLOBALS['TSFE']->fe_user->user) && $GLOBALS['TSFE']->fe_user->user['uid']) {
+            $user = $this->frontendUserRepository->findByUid(
+                (int)$GLOBALS['TSFE']->fe_user->user['uid']
+            );
+            $newPost->setFrontendUser($user);
+        } else {
+            /* normally this should never be called, because the link to create a new entry was not displayed if user was not authenticated */
+            $this->addFlashMessage('You must be logged in before creating a post');
+            $this->redirect('show', 'Forum', null, ['forum' => $topic->getForum()]);
+        }
+    }
+
+    protected function mailToUser(Post $post): void
+    {
+        $mail = $this->objectManager->get(MailMessage::class);
+        $mail->setFrom($this->extConf->getEmailFromAddress(), $this->extConf->getEmailFromName());
+        $mail->setTo($post->getUser()->getEmail(), $post->getUser()->getName());
+        $mail->setSubject(LocalizationUtility::translate('email.post.subject', 'pforum'));
+        $mail->html($this->getContentForMail($post));
+
+        $mail->send();
+    }
+
+    protected function getContentForMail(Post $post): string
+    {
+        $view = $this->objectManager->get(StandaloneView::class);
+        $view->setTemplatePathAndFilename(
+            'EXT:pforum/Resources/Private/Templates/Mail/ConfigurePost.html'
+        );
+        $view->setControllerContext($this->getControllerContext());
+        $view->assign('settings', $this->settings);
+        $view->assign('post', $post);
+
+        return $view->render();
+    }
+
+    protected function addFlashMessageForCreation(): void
+    {
+        if ($this->settings['post']['hideAtCreation']) {
+            if ($this->settings['post']['activateByAdmin']) {
+                $this->addFlashMessage(
+                    LocalizationUtility::translate('hiddenPostCreatedAndActivateByAdmin', 'pforum')
+                );
+            } else {
+                $this->addFlashMessage(
+                    LocalizationUtility::translate('hiddenPostCreatedAndActivateByUser', 'pforum')
+                );
+            }
+        } else {
+            // if topic is not hidden at creation there is no need to activate it by admin
+            $this->addFlashMessage(
+                LocalizationUtility::translate('postCreated', 'pforum')
+            );
+        }
     }
 }
--- Classes/Controller/TopicController.php
+++ Classes/Controller/TopicController.php
@@ -15,14 +15,18 @@ use JWeiland\Pforum\Domain\Model\Forum;
 use JWeiland\Pforum\Domain\Model\Topic;
 use JWeiland\Pforum\Helper\FrontendGroupHelper;
 use JWeiland\Pforum\Property\TypeConverter\UploadMultipleFilesConverter;
+use TYPO3\CMS\Core\Mail\MailMessage;
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Annotation as Extbase;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
  * Controller to list and show topics of forum
  */
-class TopicController extends AbstractTopicController
+class TopicController extends AbstractController
 {
     /**
      * @var FrontendGroupHelper
@@ -233,5 +237,94 @@ class TopicController extends AbstractTopicController
         $this->topicRepository->update($topic);
         $this->addFlashMessage(LocalizationUtility::translate('topicActivated', 'pforum'));
         $this->redirect('list', 'Forum');
+    }
+
+    /**
+     * This is a workaround to help controller actions to find (hidden) topics.
+     *
+     * @param string $argumentName
+     */
+    protected function registerTopicFromRequest(string $argumentName): void
+    {
+        $argument = $this->request->getArgument($argumentName);
+        if (is_array($argument)) {
+            // get topic from form ($_POST)
+            $topic = $this->topicRepository->findHiddenEntryByUid((int)$argument['__identity']);
+        } else {
+            // get topic from UID
+            $topic = $this->topicRepository->findHiddenEntryByUid((int)$argument);
+        }
+        $this->session->registerObject($topic, $topic->getUid());
+    }
+
+    protected function addFeUserToTopic(Forum $forum, Topic $newTopic): void
+    {
+        if (is_array($GLOBALS['TSFE']->fe_user->user) && $GLOBALS['TSFE']->fe_user->user['uid']) {
+            $user = $this->frontendUserRepository->findByUid(
+                (int)$GLOBALS['TSFE']->fe_user->user['uid']
+            );
+            $newTopic->setFrontendUser($user);
+        } else {
+            /* normally this should never be called, because the link to create a new entry was not displayed if user was not authenticated */
+            $this->addFlashMessage(
+                'You must be logged in before creating a topic',
+                '',
+                AbstractMessage::WARNING
+            );
+            $this->redirect('show', 'Forum', null, ['forum' => $forum]);
+        }
+    }
+
+    protected function mailToUser(Topic $topic): void
+    {
+        $mail = $this->objectManager->get(MailMessage::class);
+        $mail->setFrom($this->extConf->getEmailFromAddress(), $this->extConf->getEmailFromName());
+        $mail->setTo($topic->getUser()->getEmail(), $topic->getUser()->getName());
+        $mail->setSubject(
+            LocalizationUtility::translate(
+                'email.topic.subject',
+                'pforum'
+            )
+        );
+        $mail->html($this->getContentForMailing($topic));
+
+        $mail->send();
+    }
+
+    protected function getContentForMailing(Topic $topic): string
+    {
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplatePathAndFilename(
+            sprintf(
+                '%s%s',
+                ExtensionManagementUtility::extPath('pforum'),
+                'Resources/Private/Templates/Mail/ConfigureTopic.html'
+            )
+        );
+        $view->setControllerContext($this->getControllerContext());
+        $view->assign('settings', $this->settings);
+        $view->assign('topic', $topic);
+
+        return $view->render();
+    }
+
+    protected function addFlashMessageForCreation(): void
+    {
+        if ($this->settings['topic']['hideAtCreation']) {
+            if ($this->settings['topic']['activateByAdmin']) {
+                $this->addFlashMessage(
+                    LocalizationUtility::translate('hiddenTopicCreatedAndActivateByAdmin', 'pforum')
+                );
+            } else {
+                $this->addFlashMessage(
+                    LocalizationUtility::translate('hiddenTopicCreatedAndActivateByUser', 'pforum')
+                );
+            }
+        } else {
+            // if topic is not hidden at creation there is no need to activate it by admin
+            $this->addFlashMessage(
+                LocalizationUtility::translate('topicCreated', 'pforum')
+            );
+        }
     }
 }
