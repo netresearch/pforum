074ddfde187e977bed30e62de0259498b1d993b7
Stefan Froemken <froemken@gmail.com>
2023-01-20 14:22:04 +0100

    Allow TYPO3 versions higher than 11.5.16

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.32-11.5.16',
+            'typo3' => '10.4.32-11.5.99',
         ],
         'conflicts' => [
         ],
