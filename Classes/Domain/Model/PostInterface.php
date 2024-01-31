c6fb56c8ffec70e839a8b606ee36f89825f020bc
Rico Sonntag <rico.sonntag@netresearch.de>
2024-01-31 09:37:55 +0000

    Add interfaces for topic and post domain model

---
+++ Classes/Domain/Model/PostInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the package netresearch/nr-pforum-comments.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace JWeiland\Pforum\Domain\Model;
+
+/**
+ * A posting interface.
+ *
+ * @author  Rico Sonntag <rico.sonntag@netresearch.de>
+ * @license Netresearch https://www.netresearch.de
+ * @link    https://www.netresearch.de
+ */
+interface PostInterface
+{
+}
--- Classes/Domain/Model/Topic.php
+++ Classes/Domain/Model/Topic.php
@@ -12,14 +12,13 @@ declare(strict_types=1);
 namespace JWeiland\Pforum\Domain\Model;
 
 use TYPO3\CMS\Extbase\Annotation as Extbase;
-use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 /**
  * Topic model as part of a forum entry
  */
-class Topic extends AbstractEntity
+class Topic extends AbstractEntity implements TopicInterface
 {
     /**
      * @var bool
