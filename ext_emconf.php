d8b54c0f076765b7ed25258833192afd7c835fe7
Rico Sonntag <rico.sonntag@netresearch.de>
2024-02-20 07:44:09 +0000

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,23 +9,22 @@
 
 declare(strict_types=1);
 
-$EM_CONF[$_EXTKEY] = [
-    'title' => 'Forum',
-    'description' => 'Forum',
-    'category' => 'plugin',
-    'author' => 'Stefan Froemken',
-    'author_email' => 'sfroemken@jweiland.net',
-    'author_company' => 'jweiland.net',
-    'state' => 'stable',
-    'clearCacheOnLoad' => 0,
-    'version' => '4.0.3',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.32-11.5.99',
+$EM_CONF['pforum'] = [
+    'title'            => 'Forum',
+    'description'      => 'Lightweight forum extension for TYPO3',
+    'category'         => 'plugin',
+    'author'           => 'Stefan Froemken',
+    'author_email'     => 'sfroemken@jweiland.net',
+    'author_company'   => 'jweiland.net',
+    'state'            => 'stable',
+    'version'          => '4.0.3',
+    'constraints'      => [
+        'depends'   => [
+            'typo3' => '11.0.0-11.5.99',
         ],
         'conflicts' => [
         ],
-        'suggests' => [
+        'suggests'  => [
         ],
     ],
 ];
